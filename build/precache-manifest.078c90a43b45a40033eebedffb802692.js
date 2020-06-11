e6b14b9866fc4c94f6ce1dab58c052136954be5b
Marco <marcoragusa77@gmail.com>
2020-06-11 14:55:04 +0200

    adding files for github pages

---
+++ build/precache-manifest.078c90a43b45a40033eebedffb802692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4bc4689a8df62bcd2cba0cf4ef4215",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4edc03b69f140bf7d877",
+    "url": "/monsters-rolodex/static/css/main.1c0fc9e5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4edc03b69f140bf7d877",
+    "url": "/monsters-rolodex/static/js/main.171a56df.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
